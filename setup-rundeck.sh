16c5c49b87a6a48c45383c2b9f5f243461503e43
Scott Reasoner <sreasoner@outlook.com>
2026-01-12 00:27:14 -0500

    add temp bin folder fix

--- setup-rundeck.sh
+++ setup-rundeck.sh
@@ -10,4 +10,7 @@ DLPATH="$CWD/$DLNAME"
 BLDPATH="$CWD/build"
 BINPATH="$CWD/bin"
 
+# The following command is temporary to ensure bin directory exists
+mkdir -p $BINPATH
+
 cp $BLDPATH/rundeck-$RDECK_VERSION/rundeckapp/build/libs/rundeck-$RDECK_VERSION-SNAPSHOT.war $BINPATH/rundeck.war
