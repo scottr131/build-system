f66ec401a7fb149d69b36a1e4c1bc7de366347ce
Scott Reasoner <sreasoner@outlook.com>
2026-01-22 08:55:28 -0500

    start Authelia in rev proxy script

--- start-reverse-proxy.sh
+++ start-reverse-proxy.sh
@@ -7,7 +7,14 @@ CWD=$(pwd)
 
 cd $AUTHPATH
 
+# Start Authelia
+$BINPATH/authelia --config config.yml >> $LOGPATH/authelia.log 2>&1 &
+PID=$!
+echo $PID > $LOGPATH/authelia.pid
+
 # Start an instance of Caddy on port 8000 serving Homer dashboard from www directory
 sudo --preserve-env=LOGPATH $BINPATH/caddy run >> $LOGPATH/caddy-rp.log 2>&1 &
 PID=$!
 echo $PID > $LOGPATH/caddy-rp.pid
+
+cd $CWD
